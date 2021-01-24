145d51f9f80e2af3c4a7d8399695b83fd0d0e70b
APJifengc <1500533774@qq.com>
2021-01-24 19:57:13 +0800

    :tada: Init: YaAddition

---
+++ src/main/java/io/github/apjifengc/yaaddition/YaAddition.java
@@ -0,0 +1,17 @@
+package io.github.apjifengc.yaaddition;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class YaAddition extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
